882c156d3472b71db1badf8abb9fdc3ba8dd038e
rajendranneil19 <rajendranneil19@gmail.com>
2018-02-23 11:36:39 -0500

    added the post route for places traveled

--- controllers/placesTraveled.js
+++ controllers/placesTraveled.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 const User = require('../models/users')
 const placesTraveled = require('../models/placesTraveled')
 
@@ -13,52 +13,63 @@ const placesTraveled = require('../models/placesTraveled')
 //       console.log(user)
 //     res.render('placesTraveled/', {
 //         places: user.placesTraveled
-        
-    
-        
+
+
+
 //     })
 // })
 // })
 
 
 
-router.get('/new' ,(req, res) => {
-  res.render('placesTraveled/new', {
-      userId: req.params.userId
-  })
+router.get('/new', (req, res) => {
+    res.render('placesTraveled/new', {
+        userId: req.params.userId
+    })
 })
 
 
 
 router.post('/', (req, res) => {
-  User.findById(req.params.userId).then((user) => {
-      const placesTraveled = new PlacesTraved ({
-        name: req.body.name 
-      })
-  }
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      username: req.body.username
-  })
-  newUser.save().then((savedUser) => {
+    User.findById(req.params.userId).then((user) => {
+        const newplacesTraveled = new PlacesTraved({
+            state: req.body.state,
+            location: req.body.location,
+            season: req.body.seaso,
+            image: req.body.image,
+            image1: req.body.image1,
+            image2: req.bodyimagd2,
+            review: req.body.review
+        })
+        user.placestraveled.push(newplacesTraveled)
 
-      res.redirect(`/users/${savedUser._id}`)
 
-  }).catch((err) => {
-      conosle.log(err)
-  })
+        return placesTraveled.save()
+    }).then((updatedUser) => {
+        res.redirect(`/users/${req.params.userID}`)
+    })
+
 })
 
+newUser.save().then((savedUser) => {
+
+    res.redirect(`/users/${savedUser._id}`)
+
+}).catch((err) => {
+    conosle.log(err)
+})
+
+
 router.get('/:id', (req, res) => {
-  User.findById(req.params.id).then((user) => {
-    console.log("param.id is", req.params.id)
-    console.log("User is", user)
-      const newuser = user
-      console.log("newuser is", newuser)
-      res.render('users/show', {
-          newuser: newuser
-      })
-  })
+    User.findById(req.params.id).then((user) => {
+        console.log("param.id is", req.params.id)
+        console.log("User is", user)
+        const newuser = user
+        console.log("newuser is", newuser)
+        res.render('users/show', {
+            newuser: newuser
+        })
+    })
 })
 
 module.exports = router;
